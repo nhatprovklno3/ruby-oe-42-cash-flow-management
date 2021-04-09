02551bd3c920a422c0931b284db30041d99420f7
nhatprovklno3 <nguyenhongnhat71298@gmail.com>
2021-04-09 09:01:01 +0700

    gem divise nhat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,4 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: :show
-
   def show; end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new user_params
-    if @user.save
-      flash[:success] = t "flash.create_account_success"
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit :name, :email,
-                                 :password, :password_confirmation
-  end
 end
--- db/migrate/20210324022043_create_users.rb
+++ db/migrate/20210324022043_create_users.rb
@@ -2,10 +2,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password_digest
-      t.string :remember_digest
-      t.string :reset_digest
       t.datetime :reset_sent_at
       t.integer :role, default: 0
       t.boolean :status, default: true
